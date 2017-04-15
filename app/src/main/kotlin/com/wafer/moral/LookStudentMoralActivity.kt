85cab958b8a075da4f8a531f6076d5bd93dec5c5
Wafer Li <omyshokami@gmail.com>
2017-04-15 20:53:23 +0900

    Finish network deployment

--- app/src/main/kotlin/com/wafer/moral/LookStudentMoralActivity.kt
+++ app/src/main/kotlin/com/wafer/moral/LookStudentMoralActivity.kt
@@ -3,7 +3,11 @@ package com.wafer.moral
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.wafer.moral.model.response.ActivitiesResponse
 import kotlinx.android.synthetic.main.activity_look_student_moral.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LookStudentMoralActivity : AppCompatActivity() {
 
@@ -17,15 +21,20 @@ class LookStudentMoralActivity : AppCompatActivity() {
             if (studentId.isNullOrBlank())
                 look_student_id.error = getString(R.string.student_id_not_null)
             else {
-                val student = TestData.userMoralData.find { !it.isTeacher && it.id == studentId }
+                ApiManager.service.getActivities(studentId).enqueue(object : Callback<ActivitiesResponse> {
+                    override fun onFailure(call: Call<ActivitiesResponse>?, t: Throwable?) {
+                    }
 
-                if (student == null)
-                    look_student_id.error = getString(R.string.no_student)
-                else {
-                    val intent = Intent(this, MoralLookingActivity::class.java)
-                    intent.putExtra(Constants.STUDENT_ID, studentId)
-                    startActivity(intent)
-                }
+                    override fun onResponse(call: Call<ActivitiesResponse>?, response: Response<ActivitiesResponse>?) {
+                        if (response != null && response.isSuccessful) {
+                            val intent = Intent(this@LookStudentMoralActivity, MoralLookingActivity::class.java)
+                            intent.putExtra(Constants.STUDENT_ID, studentId)
+                            startActivity(intent)
+                        } else {
+                            look_student_id.error = getString(R.string.no_student)
+                        }
+                    }
+                })
             }
         }
     }
