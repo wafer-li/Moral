fab0a1c70c31c70564eeeb8cfb86c10b0a41d81e
Wafer Li <omyshokami@gmail.com>
2017-04-15 20:05:02 +0900

    Add login and get activities API

--- app/src/main/kotlin/com/wafer/moral/Api.kt
+++ app/src/main/kotlin/com/wafer/moral/Api.kt
@@ -1,5 +1,14 @@
 package com.wafer.moral
 
+import com.wafer.moral.model.request.LoginRequest
+import com.wafer.moral.model.response.ActivitiesResponse
+import com.wafer.moral.model.response.LoginResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
 /**
  * The Api class
  * Please put more info here.
@@ -7,5 +16,10 @@ package com.wafer.moral
  * @since 17/4/15 17:35
  */
 interface Api {
+    @PUT("login")
+    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+
+    @GET("activity/{student_id}/{limit_num}")
+    fun getActivites(@Path("student_id") id: String, @Path("limit_num") limitNumber: Int = 10): Call<ActivitiesResponse>
 
 }
