58d8b7210dd15336abc688a8bcd4eb7368279ea9
Wafer Li <omyshokami@gmail.com>
2017-04-15 18:54:08 +0900

    Add API manager

---
+++ app/src/main/kotlin/com/wafer/moral/Api.kt
@@ -0,0 +1,11 @@
+package com.wafer.moral
+
+/**
+ * The Api class
+ * Please put more info here.
+ * @author wafer
+ * @since 17/4/15 17:35
+ */
+interface Api {
+
+}
---
+++ app/src/main/kotlin/com/wafer/moral/ApiManager.kt
@@ -0,0 +1,39 @@
+package com.wafer.moral
+
+import android.content.Context
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * The ApiManager class
+ * Please put more info here.
+ * @author wafer
+ * @since 17/4/15 17:21
+ */
+object ApiManager {
+
+    const val BASE_URL = "http://101.200.144.204:23333/"
+
+    private lateinit var context: Context
+
+    lateinit var retrofit: Retrofit
+        private set
+
+    lateinit var service: Api
+        private set
+
+    private val gson = GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()
+
+    fun init(context: Context) {
+        this.context = context
+
+        retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+
+        service = retrofit.create(Api::class.java)
+    }
+}
